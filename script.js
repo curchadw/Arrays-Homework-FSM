17c90361be3228d074c5dfa1947dbfb45f56d7a7
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2021-03-26 03:39:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//I declared my item variable but did not assign it
+let item;
+//my myFavoriteFood array declared and iniatlly empty
+let myFavoriteFood = []
+
+//I created a food function that lets me enter an item  to put in the myFavoriteFood array using the push instance method
+const food = (item) =>{
+  //If the item already exist in the array, it will not duplicate the item in the array
+  (myFavoriteFood.includes(item) === false) ? myFavoriteFood.push(item) :  console.log('This already exists in this collection')
+
+  return `My Food Array ---> ${myFavoriteFood}`
+}
+
+//I created a function that check to see if a third item exist in the array account for both exisiting and not exisitng 
+const thirdItem = () =>{
+  myFavoriteFood[2] ? console.log(myFavoriteFood[2]) : console.log('There is no third item in collection')
+
+  return myFavoriteFood[2] || 'Not present'
+}
+
+//Just for kicks this function allows me to change the third item and returns the modified array
+const changeThirdItem = (item) =>{
+  myFavoriteFood[2] ? myFavoriteFood[2] = item : ''
+  return myFavoriteFood
+}
