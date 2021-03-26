22fee4e45ec035eaf1dbc0b73cf3d891793d972e
Curtis Laurence Chadwell <43461325+curchadw@users.noreply.github.com>
2021-03-26 03:46:46 +0000

    I modified some grammatical errors

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let myFavoriteFood = []
 
 //I created a food function that lets me enter an item  to put in the myFavoriteFood array using the push instance method
 const food = (item) =>{
-  //If the item already exist in the array, it will not duplicate the item in the array
+  //If the item already exists in the array, it will not duplicate the item in the array
   (myFavoriteFood.includes(item) === false) ? myFavoriteFood.push(item) :  console.log('This already exists in this collection')
 
   return `My Food Array ---> ${myFavoriteFood}`
 }
 
-//I created a function that check to see if a third item exist in the array account for both exisiting and not exisitng 
+//I created a function that check to see if a third item exist in the array accounts for both exisiting and non exisitng 
 const thirdItem = () =>{
   myFavoriteFood[2] ? console.log(myFavoriteFood[2]) : console.log('There is no third item in collection')
 
